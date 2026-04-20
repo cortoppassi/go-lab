8f9421960e87250fd86c27351e7fdf1332638b40
jonathan cortoppassi <jonathan-516@hotmail.com>
2026-04-20 16:15:57 -0300

    adiciona estrutura Course e imprime detalhes do curso no início do servidor

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import "net/http"
+type Course struct {
+	Name string
+	Instructor string
+	Price int
+}
 
 func main() {
+	course:= Course{
+		Name: "Go Programming",
+		Instructor: "John Doe",
+		Price: 100,
+	}
+	println("Course Name:", course.Name)
+	println("Instructor:", course.Instructor)
+	println("Price:", course.Price)
+	
 	http.HandleFunc("/", homeHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, World!"))
-}
+} 
